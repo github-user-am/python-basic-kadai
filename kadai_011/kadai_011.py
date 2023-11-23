7cd070b21c97181db0d5d3c3360f090ad9350fd9
github-user-am <149214019+github-user-am@users.noreply.github.com>
2023-11-23 10:17:52 +0900

    Add files via upload

---
+++ kadai_011/kadai_011.py
@@ -0,0 +1,14 @@
+list = ["水", "金", "地", "火", "木", "土", "天", "海", "冥"]
+
+# for文で
+for value in list:
+    print(value)
+
+# while文で
+list_length = len(list)
+i = 0
+
+while i < len(list):
+    print(list[i])
+    i = i + 1
+
